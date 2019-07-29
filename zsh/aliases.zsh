73e3034d3525836f9ff8f8dc0e07cf0ef09ebdb6
Matthew <matteidsen@gmail.com>
2019-07-28 18:21:53 -0700

    fixed alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias ....='cd ../../..'
 
 # colorizses some output... I prefer this
 alias ls='ls --color=auto'
-alias la='ls -a --color=auto'
+alias la='ls -A --color=auto'
 alias ll='ls -alF --color=auto'
 
 alias grep='grep --color=auto'
