21df1d51288b0d7975ee87a0cc2e3855d397e7b4
Matthew <matteidsen@gmail.com>
2019-10-13 20:19:41 -0700

    fixed vim syntax highlighting while in tmux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,13 @@
 # these are my standard system aliases
 
-# quick moving
+# quick moving and other stuff
+
+alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-alias --='cd -'
+alias h='history'
 
 # colorizses some output... I prefer this
 alias ls='ls --color=auto'
@@ -13,6 +15,20 @@ alias la='ls -A --color=auto'
 alias ll='ls -alF --color=auto'
 
 alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias diff='colordiff'
+
+# file actions
+
+alias mkdir='mkdir -pv'
+alias edit='vim'
+
+# system actions
+
+alias shutdown='sudo shutdown -h now'
+alias reboot='sudo shutdown -r now'
 
 # updates
 alias update='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
