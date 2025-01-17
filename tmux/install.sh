f8410281fe5eadfd6055d449026ef72435c32d34
Matthew <matteidsen@gmail.com>
2025-01-16 22:10:26 -0800

    remove tmux, not actively using

--- tmux/install.sh
+++
@@ -1,6 +0,0 @@
-if [ $(dpkg-query -W -f='${Status}' tmux 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-    sudo apt install tmux -y;
-else
-    echo "tmux is already installed... moving on"
-fi
