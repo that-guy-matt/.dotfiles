efb5e7bf4745274d3387f1f4131b1c5599c83af7
Matthew <matteidsen@gmail.com>
2025-01-16 21:59:57 -0800

    remove powerlevel9k, no longer using and will probably update with different terminal theme later

--- powerlevel9k/install.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "$HOME/powerlevel9k" ]; then
-	echo "Installing powerlevel9k theme..."
-	if test $(which apt); then
-		sudo apt install fonts-powerline -y
-		git clone https://github.com/bhilburn/powerlevel9k.git $HOME/powerlevel9k
-	fi
-else
-	echo "Powerlevel9k already exists"
-fi
