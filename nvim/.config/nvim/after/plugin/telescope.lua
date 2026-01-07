857302b6f0f3064dfcf39f2132b3cb2ed101e003
Matthew Eidsen <mattheweidsen@Matthews-MacBook-Air-2.local>
2026-01-07 09:23:29 -0600

    fix telescope missing builtin

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,12 @@
+local builtin = require 'telescope.builtin'
+
 --vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files {
+    hidden = true,
+    no_ignore = true,
+  }
+end)
 
 -- search in git repo
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
@@ -6,12 +14,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git fil
 -- live grep
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string { search = vim.fn.input 'Grep > ' }
-end)
-
--- find files
-vim.keymap.set('n', '<leader>ff', function()
-  builtin.find_files {
-    hidden = true,
-    no_ignore = true,
-  }
 end)
