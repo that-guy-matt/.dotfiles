51465ebac6d542717474e75799eba5004967c4ec
Matthew <matteidsen@gmail.com>
2026-01-26 14:25:46 -0600

    add zinit plugins for syntax highlighting, autocomplete, alias reminders

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,3 +1,16 @@
-# Powerlevel10k theme (prompt)
+# ===== Powerlevel10k theme (prompt) =====
+zinit ice depth=1 lucid
 zinit light romkatv/powerlevel10k
+
+# ===== Autosuggestions =====
+zinit ice depth=1 wait lucid
+zinit light zsh-users/zsh-autosuggestions
+
+# ===== You-should-use =====
+zinit ice depth=1 wait lucid
+zinit light MichaelAquilina/zsh-you-should-use
+
+# ===== Syntax highlighting (MUST load last) =====
+zinit ice depth=1 lucid
+zinit light zsh-users/zsh-syntax-highlighting
 
