baa8f8e353dfcc94f5df2d8e6a709a3afe53dc94
Matthew <matteidsen@gmail.com>
2026-01-27 17:37:37 -0600

    nvim: add treesitter plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,45 @@
--- ~/.config/nvim/init.lua
+--        -- Shell/CLI
+        'bash',
+        'zsh',
+
+        -- Systems / Scripting / General purpose
+        'c',
+        'java',
+        'cpp',
+        'lua',
+        'python',
+
+        -- Web
+        'html',
+        'javascript',
+        'css',
+        'scss',
+        'typescript',
+        'tsx',
+
+        -- Data / Databases
+        'sql',
+        'prisma',
+
+        -- Configs
+        'json',
+        'yaml',
+        'toml',
+        'ini',
+        'dockerfile',
+        'git_config',
+        'gitignore',
+        'diff',
+
+        -- Docs / Markup
+        'markdown',
+        'markdown_inline',
+
+        -- Neovim / Editor Internals
+        'luadoc',
+        'query',
+        'vim',
+        'vimdoc', ~/.config/nvim/init.lua
 -- ===============================================
 -- This is the single entry point for Neovim
 -- It loads the main custom configuration module
---
+++ nvim/.config/nvim/lua/nesdiem/plugins/treesitter.lua
@@ -0,0 +1,85 @@
+-- ~/.config/nvim/lua/nesdiem/plugins/treesitter.lua
+-- ============================================================
+-- nvim-treesitter plugin spec for Lazy.nvim
+-- Provides syntax highlighting, indentation, and textobject support
+-- ============================================================
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    name = "treesitter",
+    -- Run :TSUpdate after installing to ensure parsers are up-to-date
+    build = ":TSUpdate",
+    -- Load immediately since highlighting and indentation are core features
+    lazy = false,
+    priority = 900,
+
+    opts = {
+      -- ==========================================================
+      -- List of languages you want installed automatically
+      -- Can be expanded as needed
+      -- ==========================================================
+      ensure_installed = {
+        -- Shell/CLI
+        'bash',
+        'zsh',
+
+        -- Systems / Scripting / General purpose
+        'c',
+        'java',
+        'cpp',
+        'lua',
+        'python',
+
+        -- Web
+        'html',
+        'javascript',
+        'css',
+        'scss',
+        'typescript',
+        'tsx',
+
+        -- Data / Databases
+        'sql',
+        'prisma',
+
+        -- Configs
+        'json',
+        'yaml',
+        'toml',
+        'ini',
+        'dockerfile',
+        'git_config',
+        'gitignore',
+        'diff',
+
+        -- Docs / Markup
+        'markdown',
+        'markdown_inline',
+
+        -- Neovim / Editor Internals
+        'luadoc',
+        'query',
+        'vim',
+        'vimdoc',
+      },
+
+      -- ==========================================================
+      -- Enable Treesitter-based syntax highlighting
+      -- ==========================================================
+      highlight = {
+        enable = true,          -- enable treesitter highlighting
+        additional_vim_regex_highlighting = false, -- do not double-highlight with regex
+      },
+
+      -- ==========================================================
+      -- Enable Treesitter-based indentation
+      -- ==========================================================
+      indent = {
+        enable = true,
+        disable = { "python" }, -- optional: disable for languages that misbehave
+      },
+    },
+  },
+}
+
